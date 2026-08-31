f66a0e40c120344a520b9fc170e7c92c883c00ff
Lucas Costa de Paula <112268997+lucasvalendev@users.noreply.github.com>
2026-08-31 16:06:13 -0300

    Add app/sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,3 @@
+import type { MetadataRoute } from 'next'
+export default function sitemap(): MetadataRoute.Sitemap { return [{ url: 'https://green-wash-demo.vercel.app', lastModified: new Date('2026-08-31') }] }
+
